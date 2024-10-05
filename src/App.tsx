0081b8c8b2d3fc71eded201a8062d03800481aec
dkhoanguyen1101 <dkhoa.nguyen1101@gmail.com>
2024-10-05 06:32:08 -0600

    fix:containment

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import Greeting from "./components/Greeting.tsx";
 import Action1 from "./components/Action1.tsx";
 
 function App() {
-  useEffect(() => {
-    document.body.classList.add("m-0");
-  });
-  return (
-    <div className="App">
-      <Navbar />
-      <Greeting />
-      <Action1 />
-    </div>
-  );
+    useEffect(() => {
+        document.body.classList.add("m-0");
+    });
+    return (
+        <div className="App">
+            <div className="app-container">
+                <Navbar />
+                <Greeting />
+                <Action1 />
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Action1.tsx
+++ src/components/Action1.tsx
@@ -1,12 +1,12 @@
 import "@/styles/components/Action.sass";
 
 export default function Action1() {
-  return (
-    <div className="action">
-      <p>Come on, I'll show you around</p>
-      <div className="mouse-icon">
-        <div className="wheel"></div>
-      </div>
-    </div>
-  );
+    return (
+        <div className="action">
+            <p>C'mon, I'll show you around</p>
+            <div className="mouse-icon">
+                <div className="wheel"></div>
+            </div>
+        </div>
+    );
 }
