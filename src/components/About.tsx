72792d816e15c1af9a3b00bb87f9bbce705534e9
dkhoanguyen1101 <dkhoa.nguyen1101@gmail.com>
2024-10-08 22:21:21 -0600

    icons

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,6 +3,7 @@ import PCModel from "./PCModel";
 import { IoLogoPython } from "react-icons/io";
 import { BiLogoTypescript } from "react-icons/bi";
 import DropdownList, { DropdownListItem } from "./utils/DropdownList";
+import { TbBrandCpp } from "react-icons/tb";
 
 const DynamicTitle = () => {
     const words = ["Khoa", "graduating in 2025", "a tinkerer", "an explorer", "a learner"];
@@ -55,7 +56,9 @@ export default function About() {
                 <DynamicTitle />
                 <p><b>My Tech Stack:</b></p>
                 <ul>
-                    <li>Language: <IoLogoPython style={{ cursor: "pointer" }} />, <BiLogoTypescript style={{ cursor: "pointer" }} />, C++</li>
+                    <li>
+                        Language: <IoLogoPython />,
+                        <BiLogoTypescript />, <TbBrandCpp /></li>
                     <li>Tools: Git, neovim, Docker</li>
                     <li>Cloud: AWS, S3.</li>
                 </ul>
@@ -63,8 +66,16 @@ export default function About() {
                 <DropdownList items={experiences} />
                 <p><b>Competitions</b></p>
                 <ul>
-                    <li>2<sup>nd</sup> place in HTC 2021</li>
-                    <li>Best use of Virtual Reality at CalgaryHacks 2022</li>
+                    <li style={{ listStyleType: "'\\1F948" }}>
+                        <a href="https://devpost.com/software/moosique" target="_blank" rel="noopener noreferrer">
+                            2<sup>nd</sup> place in HTC 2021
+                        </a>
+                    </li>
+                    <li style={{ listStyleType: "'\\1F3C6'" }}>
+                        <a href="https://devpost.com/software/hymalayas" target="_blank" rel="noopener noreferrer">
+                            Best use of Virtual Reality at CalgaryHacks 2022
+                        </a>
+                    </li>
                 </ul>
                 <p><b>Currently learning about:</b></p>
                 <ul>
@@ -74,6 +85,6 @@ export default function About() {
                     <li>3D Modeling</li>
                 </ul>
             </div>
-        </div>
+        </div >
     );
 }
