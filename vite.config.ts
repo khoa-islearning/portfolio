bfc5a09aa7a2ba034d72fc8a453cb913d86553ab
dkhoanguyen1101 <dkhoa.nguyen1101@gmail.com>
2024-10-18 01:47:05 -0600

    test new out dir

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
-        outDir: 'dist',
+        outDir: 'build_dist',
     },
     resolve: {
         alias: {
